6261e3d38927b52686dc5214d880a369130db3d0
john muhl <git@johnmuhl.com>
2017-12-14 15:47:25 -0600

    be prettier

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const {app, BrowserWindow, crashReporter} = require('electron')
-const path = require('path')
-const url = require('url')
-let win
+const { app, BrowserWindow, crashReporter } = require("electron");
+const path = require("path");
+const url = require("url");
+let win;
 
 crashReporter.start({
-	autoSubmit: true,
-	companyName: 'Electron crash report server',
-	extra: {extra: 'info from the main process'},
-	ignoreSystemCrashHandler: true,
-	submitURL: 'https://pacific-falls-32011.herokuapp.com/',
-})
+  autoSubmit: true,
+  companyName: "Electron crash report server",
+  extra: { extra: "info from the main process" },
+  ignoreSystemCrashHandler: true,
+  submitURL: "https://pacific-falls-32011.herokuapp.com/"
+});
 
-function createWindow () {
-	win = new BrowserWindow({height: 600, width: 800})
+function createWindow() {
+  win = new BrowserWindow({ height: 600, width: 800 });
 
-	win.loadURL(
-		url.format({
-			pathname: path.join(__dirname, 'index.html'),
-			protocol: 'file',
-		})
-	)
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file"
+    })
+  );
 
-	win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
-	win.on('closed', () => {
-		win = null
-	})
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', () => {
-	if (win === null) createWindow()
-})
+app.on("activate", () => {
+  if (win === null) createWindow();
+});
