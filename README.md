f03a981958b6e8441af919382d1e2ffa28f94ea3
john muhl <git@johnmuhl.com>
2019-05-09 08:04:27 -0500

    chore: arbitrary version bump

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 An Electron application that generates crash reports and sends them to an
 [Electron crash report server].
 
-[electron crash report server]: https://github.com/johnmuhl/electron-crash-report-server
+[electron crash report server]:
+	https://github.com/johnmuhl/electron-crash-report-server
--- index.js
+++ index.js
@@ -5,42 +5,42 @@ const url = require("url");
 let win;
 
 crashReporter.start({
-  companyName: "Electron crash report server",
-  extra: { extra: "info from the main process" },
-  ignoreSystemCrashHandler: true,
-  submitURL: "https://pacific-falls-32011.herokuapp.com/",
-  uploadToServer: true,
+	companyName: "Electron crash report server",
+	extra: { extra: "info from the main process" },
+	ignoreSystemCrashHandler: true,
+	submitURL: "https://pacific-falls-32011.herokuapp.com/",
+	uploadToServer: true,
 });
 
 function createWindow() {
-  win = new BrowserWindow({
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    width: 800,
-  });
+	win = new BrowserWindow({
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+		width: 800,
+	});
 
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file",
-    })
-  );
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname, "index.html"),
+			protocol: "file",
+		})
+	);
 
-  win.webContents.openDevTools();
+	win.webContents.openDevTools();
 
-  win.on("closed", () => {
-    win = null;
-  });
+	win.on("closed", () => {
+		win = null;
+	});
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", () => {
-  if (win === null) createWindow();
+	if (win === null) createWindow();
 });
